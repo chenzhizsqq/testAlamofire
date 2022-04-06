0bc71f6c96429a3cdb626ada7dd07dd4a8dbae7d
chenzhizs <chenzhi@qq.com>
2022-04-06 15:57:53 +0900

    参照多个例子，做好了一个最简单的sample https://mo-gu-mo-gu.com/ios-alamofire/

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'testAlamofire' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for project
+    pod 'SwiftyJSON'
+    pod 'Alamofire'
+
+  # Pods for testAlamofire
+
+end
--- testAlamofire/ViewController.swift
+++ testAlamofire/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
     //test
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // AlamofireでAPIリクエストをする
+        AF.request("https://qiita.com/api/v2/items")
+            // レスポンスをJSON形式で受け取る
+            .responseJSON { response in
+                switch response.result {
+                case .success:
+                    let decoder: JSONDecoder = JSONDecoder()
+                    do {
+                        // decode関数の引数にはJSONからマッピングさせたいクラスをと実際のデータを指定する
+                        let articles: [Article] = try decoder.decode([Article].self, from: response.data!)
+                        print(articles)
+                    } catch {
+                        // JSONの形式とクラスのプロパティが異なっている場合には失敗する
+                        print("failed")
+                        print(error.localizedDescription)
+                    }
+                case .failure(let error):
+                    print("error", error)
+                }
+        }
     }
-
 
 }
 
