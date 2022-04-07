b9c6d104f367ec446b80f1a8b1b613dd9f552869
chenzhizs <chenzhi@qq.com>
2022-04-07 13:13:29 +0900

    添加了SwiftyJSON的处理方式

--- testAlamofire/ViewController.swift
+++ testAlamofire/ViewController.swift
@@ -10,13 +10,33 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController {
-    //test
+    
+    //显示测试结果的TextView
+    @IBOutlet weak var testTextView: UITextView!
+    
+    @IBOutlet weak var buttonAlamofire: UIButton!
+    
+    @IBOutlet weak var buttonSwiftyJSON: UIButton!
+    
+    @IBAction func touchSwiftyJSONListen(_ sender: UIButton) {
+        testSwiftyJSON()
+    }
+    
+    @IBAction func touchAlamofireListen(_ sender: Any) {
+        testAlamofire()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
+    }
+    
+    func testAlamofire()  {
         // AlamofireでAPIリクエストをする
         AF.request("https://qiita.com/api/v2/items")
-            // レスポンスをJSON形式で受け取る
+        // レスポンスをJSON形式で受け取る
             .responseJSON { response in
                 switch response.result {
                 case .success:
@@ -25,6 +45,15 @@ class ViewController: UIViewController {
                         // decode関数の引数にはJSONからマッピングさせたいクラスをと実際のデータを指定する
                         let articles: [Article] = try decoder.decode([Article].self, from: response.data!)
                         print(articles)
+                        
+                        //显示测试的结果
+                        self.testTextView.text =  articles.first?.title
+                        
+                        for i in (0 ..< articles.count) {
+                            print("articles[i].title:" + articles[i].title)
+                            print("articles[i].user.id:" + articles[i].user.id)
+                        }
+                        
                     } catch {
                         // JSONの形式とクラスのプロパティが異なっている場合には失敗する
                         print("failed")
@@ -33,8 +62,23 @@ class ViewController: UIViewController {
                 case .failure(let error):
                     print("error", error)
                 }
-        }
+            }
     }
 
+    
+    func testSwiftyJSON(){
+        
+        let url = URL(string: "https://pythonchannel.com/media/codecamp/201902/JSON-Sample3.json")
+
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            guard let data = data else {return}
+            do {
+                let json = try? JSON(data: data)
+                let jsonData = json!["posts"][0]["name"]
+                print("test SwiftyJSON !!!!!")
+                print(jsonData)
+            }
+            }.resume()
+    }
 }
 
